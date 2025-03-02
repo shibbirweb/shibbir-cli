d1b5a2c9b73b7f3ed9668ac489551b2384287abc
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2025-03-02 15:05:29 +0600

    feat:[#1]: Fixed #1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
+import inquirer from "inquirer";
+import { nodeMain } from "./node";
 
-console.log("Greeting from Shibbir's CLI!");
+console.log("Hello from Shibbir's CLI!");
+
+const TYPE_NODE = "Node";
+
+async function main() {
+  const { type } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "type",
+      message: "Select an option:",
+      choices: [TYPE_NODE],
+    },
+  ]);
+
+  switch (type) {
+    case TYPE_NODE:
+      await nodeMain();
+      break;
+  }
+}
+
+main();
---
+++ src/node/package-development/index.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+import register from "./register";
+
+export default async function () {
+  const { type } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "type",
+      message: "Select an option:",
+      choices: [...Object.keys(register), "Exit"],
+    },
+  ]);
+
+  if (type === "Exit") {
+    console.log("Exiting...");
+    return;
+  }
+
+  const command = register[type]
+
+  if (command) {
+    await command();
+  } else {
+    console.log("Command not found!");
+  }
+}
